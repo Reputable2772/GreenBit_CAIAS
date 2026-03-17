aff1414e5f841694c506a5ea9f9749a0ec55b70b
Reputable2722 <153411261+Reputable2772@users.noreply.github.com>
2026-03-17 16:04:36 +0530

    fix: install runtime libraries for python
    Signed-off-by: Reputable2722 <153411261+Reputable2772@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -10,13 +10,31 @@
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+      runtimeLibs = with pkgs; [
+        stdenv.cc.cc.lib
+        zlib
+        glibc
+      ];
     in
     {
       devShells.x86_64-linux.default = pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [
-          python314
-          nixfmt
-        ];
+        nativeBuildInputs =
+          with pkgs;
+          [
+            python315
+            python315Packages.venvShellHook
+            nixfmt
+          ]
+          ++ runtimeLibs;
+
+        shellHook = ''
+          export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath runtimeLibs}:$LD_LIBRARY_PATH"          echo "Python environment loaded. libstdc++ is now in LD_LIBRARY_PATH."
+
+          # Optional: Automatically enter your venv if it exists
+          if [ -d ".venv" ]; then
+            source .venv/bin/activate
+          fi
+        '';
       };
     };
 }
